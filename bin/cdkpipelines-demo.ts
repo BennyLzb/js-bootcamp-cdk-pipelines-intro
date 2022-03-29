9d4d225fbb93eb09bcd92598b9cd50e7adedf1d9
bennyli <bli@junglescout.com>
2022-03-29 16:17:09 +0800

    Add PreProd stage

--- bin/cdkpipelines-demo.ts
+++ bin/cdkpipelines-demo.ts
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
-import { App } from "@aws-cdk/core";
-import { CdkpipelinesDemoPipelineStack } from "../lib/cdkpipelines-demo-pipeline-stack";
+import { App } from '@aws-cdk/core';
+import { CdkpipelinesDemoPipelineStack } from '../lib/cdkpipelines-demo-pipeline-stack';
 
 const app = new App();
 
-new CdkpipelinesDemoPipelineStack(app, "CdkpipelinesDemoPipelineStack", {
-  env: { account: "ACCOUNT", region: "us-east-1" },
+new CdkpipelinesDemoPipelineStack(app, 'CdkpipelinesDemoPipelineStack', {
+  env: { account: '746252282333', region: 'us-east-1' },
 });
 
 app.synth();
--- lib/cdkpipelines-demo-pipeline-stack.ts
+++ lib/cdkpipelines-demo-pipeline-stack.ts
@@ -4,6 +4,7 @@ import {
   CodePipelineSource,
   ShellStep,
 } from '@aws-cdk/pipelines';
+import { CdkpipelinesDemoStage } from './cdkpipelines-demo-stage';
 
 /**
  * The stack that defines the application pipeline
@@ -31,5 +32,10 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
 
     // This is where we add the application stages
     // ...
+    pipeline.addStage(
+      new CdkpipelinesDemoStage(this, 'PreProd', {
+        env: { account: '746252282333', region: 'us-east-1' },
+      })
+    );
   }
 }
